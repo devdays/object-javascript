131e825bd148a205a94bc3bcdba6e701b6f05425
Bruce Kyle <brucedkyle@gmail.com>
2014-12-16 20:11:09 -0800

    Initial checkin

---
+++ RequireJSSample/Scripts/discount.js
@@ -0,0 +1,22 @@
+﻿define('discount',
+    function () {
+        // properties
+        var pct = 0,
+        // methods
+        calculateDiscount = function (level) {
+            switch (level) {
+                case 'partner':
+                    pct = 10;
+                    break;
+                case 'employee':
+                    pct = 27;
+                    break;
+                default:
+                    pct = 0;
+            }
+            return pct;
+        }
+        return {
+            calculateDiscount: calculateDiscount
+        };
+    });
---
+++ RequireLoDashTemplates/Scripts/app/app.js
@@ -0,0 +1,21 @@
+﻿require.config({
+    // path begins at the current location
+    paths: {
+        // you could use a CDN version of jQuery or whatever
+        // just point to it in paths
+        // "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+        "jquery": "../../Scripts/jquery-2.0.3",
+        "_": "../../Scripts/lodash",
+    }
+});
+
+
+require(
+    // use the module.template.js file 
+    ['../modules/template'],
+
+    // to call the template module and call the showName method
+    function (template) {
+        template.showName("Jack");
+});
+
